b0c17e7d937aa4aee45d709c02e187f3e31836d5
Arru <ricardo.ruz@usach.cl>
2020-09-12 03:21:26 -0300

    Reinicio Front

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# fro
+# plantillaVue
 
 ## Project setup
 ```
--- src/main.js
+++ src/main.js
@@ -1,14 +1,19 @@
 import Vue from 'vue'
 import App from './App.vue'
+import vuetify from './plugins/vuetify';
+import VueTheMask from 'vue-the-mask'
+import axios from 'axios'
+import VueAxios from 'vue-axios'
+
+Vue.use(VueAxios, axios)
+Vue.use(VueTheMask)
+Vue.config.productionTip = false
 import router from './router'
 import store from './store'
-import vuetify from './plugins/vuetify';
-
-Vue.config.productionTip = false
 
 new Vue({
-  router,
-  store,
-  vuetify,
-  render: h => h(App)
+    vuetify,
+    router,
+    store,
+    render: h => h(App)
 }).$mount('#app')
